aef84d8eb0a56e4ab353fce3c3365086fdb8cbd2
antonicarol <antonicarolgit@gmail.com>
2022-05-04 10:30:10 +0200

    added cors

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const PORT = process.env.PORT || 9000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fibbo-market.web.app/",
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
