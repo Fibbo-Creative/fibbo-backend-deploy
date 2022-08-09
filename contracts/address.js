43d96097f220fb3f47f9d9519c507a2a3490d226
antonicarol <antonicarolgit@gmail.com>
2022-08-09 10:57:49 +0200

    Done

--- contracts/address.js
+++ contracts/address.js
@@ -1,8 +1,8 @@
-const marketAddress = "0x18F80d313c5fF5152520F16d18b205A655a1DA68";
-const nftColectionAddress = "0xf162A26aEdA893f2131Eb634dE9A2Aed7E395544";
-const suggestionsAddress = "0xE80E90Ce84574830b1aD7e68C09aE82000C5014f";
-const verificationAddress = "0x360Eb0e59Ec91253a4B3704160Cd1FBa9ae5F996";
-const auctionAddress = "0x9f3f392C50A0db297230C360153258F05405a359";
+const marketAddress = "0x00Ba829061f95230f23E89BdC29ABC677855230A";
+const nftColectionAddress = "0xE99D90205cd12B5F2ec7C693bA6BF27591a35D91";
+const suggestionsAddress = "0x2F6aD1D11846140Bdeb2eb025CA0b85Bf25e217E";
+const verificationAddress = "0x95ACEb52cd4CcCA6E3097c95a1c04adD5C169AF8";
+const auctionAddress = "0x1778FfB4B3d297dbD20EE07C39acb10B39fEC71A";
 const wftmAddress = "0xf1277d1ed8ad466beddf92ef448a132661956621";
 export {
   marketAddress,
