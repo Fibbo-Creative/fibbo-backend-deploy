2a79712507539706025821b378157f539f53732c
antonicarol <antonicarolgit@gmail.com>
2022-09-12 19:33:12 +0200

    prepared dev branch

--- contracts/address.js
+++ contracts/address.js
@@ -1,4 +1,4 @@
 const wftmAddress = "0x4EEf747dC4f5d110d9bCfA5C6F24b3359bD4B2d4";
-const addressRegistry = "0x02c744763651E4a7Bd079c04d5EAA6e18a46E00d";
+const addressRegistry = "0x1C2123D5E78FE2a107c0bC76d5260393F7775DD8";
 const forwarder = "0x944f2A65742dbe3963BeDeE9d55cdF02572db6Da";
 export { wftmAddress, addressRegistry, forwarder };
