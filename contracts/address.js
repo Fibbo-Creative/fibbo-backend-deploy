638da4d304c2b41428fd6461e23378ca1384b0e2
antonicarol <antonicarolgit@gmail.com>
2022-11-11 11:27:48 +0100

    Set up branch for gallery

--- contracts/address.js
+++ contracts/address.js
@@ -1,5 +1,5 @@
 const wftmAddress = "0x2b513ea648453a3Cf6EF3A801356d54A2dB625e9";
-const addressRegistry = "0x3C70Ff5af18C8462ed32724e51213c2E6BC4e3f0";
+const addressRegistry = "0xD635a924a327d1f3B1f00BAEF476657f8635F2C7";
 const forwarder = "0x944f2A65742dbe3963BeDeE9d55cdF02572db6Da";
 const gasStation = "0x1f1175101579059a6d8a695a29650eacd6b043b0";
 export { wftmAddress, addressRegistry, forwarder, gasStation };
