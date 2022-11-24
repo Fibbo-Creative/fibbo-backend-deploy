65a95d698af988e65fe8eea87f7936fcf64dbe5e
antonicarol <antonicarolgit@gmail.com>
2022-11-24 19:10:50 +0100

    fixed error with rpc

--- contracts/address.js
+++ contracts/address.js
@@ -1,5 +1,5 @@
 const wftmAddress = "0x2b513ea648453a3Cf6EF3A801356d54A2dB625e9";
-const addressRegistry = "0x02c744763651E4a7Bd079c04d5EAA6e18a46E00d";
+const addressRegistry = "0x1C2123D5E78FE2a107c0bC76d5260393F7775DD8";
 const forwarder = "0x944f2A65742dbe3963BeDeE9d55cdF02572db6Da";
 const gasStation = "0x1f1175101579059a6d8a695a29650eacd6b043b0";
 export { wftmAddress, addressRegistry, forwarder, gasStation };
